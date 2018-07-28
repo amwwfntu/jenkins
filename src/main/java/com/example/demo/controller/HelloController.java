60092a6ade8653a39cb9ff887679575ffb865832
黄伟可 <huangweike@huangweikediMacBook-Pro.local>
2018-07-28 17:53:12 +0800

    log

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.pojo.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    private Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public Person hello(){
+        logger.info("I'm here !");
         Person person = new Person();
         person.setName("huangweike");
         person.setAge("33");
